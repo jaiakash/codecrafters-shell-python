ce0d7e301467508f76e0c3ad8663ba903ebc68fc
Akash Jaiswal <akashjaiswal3846@gmail.com>
2024-07-14 02:06:06 +0530

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ def main():
         sys.stdout.flush()
 
         command = input()
+        if command == "exit 0":
+            break
         sys.stdout.write(f"{command}: command not found\n")
 
 

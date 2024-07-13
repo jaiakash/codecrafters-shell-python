3eb425f23117e0bc337bd59c96aa7f34cfbe3f86
Akash Jaiswal <akashjaiswal3846@gmail.com>
2024-07-14 02:12:02 +0530

    pass 6th stage

--- app/main.py
+++ app/main.py
@@ -9,9 +9,20 @@ def main():
         command = input()
         if command == "exit 0":
             break
+
         if command.startswith("echo"):
             sys.stdout.write(command[5:] + "\n")
             continue
+
+        if command.startswith("type"):
+            if command[5:] == "echo" or command[5:] == "exit" or command[5:] == "type":
+                sys.stdout.write(command[5:] + " is a shell builtin\n")
+                continue
+            else:
+                sys.stdout.write(f"{command[5:]}: not found\n")
+                continue
+            continue
+
         sys.stdout.write(f"{command}: command not found\n")
 
 

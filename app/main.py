3119309b8ebb33f2c1f3a6ca0099b49fc673fb46
Akash Jaiswal <akashjaiswal3846@gmail.com>
2024-07-14 01:56:59 +0530

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ def main():
     sys.stdout.write("$ ")
     sys.stdout.flush()
 
-    # Wait for user input
-    input()
+    command = input()
+    sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
